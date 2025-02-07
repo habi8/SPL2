3eb0fedd623ca9bb4be5d7585b76179969576135
SultanaElin <bsse1445@iit.du.ac.bd>
2025-02-07 13:28:55 +0600

    Added Welcome Page

---
+++ Welcome _Page/Welcome.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.content').style.opacity = "1";
+    }, 4000);
+});
+
+function enterSite() {
+    window.location.href = 'Ocean.html';
+}
